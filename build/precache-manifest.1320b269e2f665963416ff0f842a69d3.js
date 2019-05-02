8b5c98e3fc93bc4cfb0150c89ecbb34ba7bc72f5
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-05-02 18:38:26 +0300

    added frontend build to this server package

---
+++ build/precache-manifest.1320b269e2f665963416ff0f842a69d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "43d3bc26b128349c3e7e",
+    "url": "/static/js/main.a62eaabe.chunk.js"
+  },
+  {
+    "revision": "717095a3ea21a6bbdee1",
+    "url": "/static/js/2.3d2452bb.chunk.js"
+  },
+  {
+    "revision": "43d3bc26b128349c3e7e",
+    "url": "/static/css/main.d0a1c937.chunk.css"
+  },
+  {
+    "revision": "79907f3f0820bd5df389e4cd46acbe2b",
+    "url": "/index.html"
+  }
+];
